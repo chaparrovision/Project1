bcfb860a8aad7e7ad4d2719f2f1cb35947deadd4
chaparrovision <david.chaparro@ymail.com>
2019-07-24 18:56:49 -0400

    All but 1 frame complete

--- v2 HTMLs/p0-script.js
+++ v2 HTMLs/p0-script.js
@@ -3,7 +3,11 @@ document.getElementById("loginBtn").addEventListener("click", toMainBody);
 document.getElementById("get-all-users-btn").addEventListener("click", toGetAllUsers);
 document.getElementById("create-new-reimb").addEventListener("click", toCreateNewReimb);
 document.getElementById("get-user-by-Id-btn").addEventListener("click", toGetUserById);
-document.getElementById("get-update-user-btn").addEventListener("click", toUpdateUser);//resume
+document.getElementById("get-update-user-btn").addEventListener("click", toUpdateUser);
+document.getElementById("get-reim-by-status-btn").addEventListener("click", toGetReimByStatus);
+document.getElementById("get-reim-by-type-btn").addEventListener("click", toGetReimByType);
+document.getElementById("get-all-reims-btn").addEventListener("click", toGetAllReims);
+document.getElementById("update-reims-btn").addEventListener("click", toUpdateReims);//resume
                
 function toMainBody() {
     document.getElementById("indexContentDiv").style.display="none";
@@ -18,13 +22,22 @@ function toGetAllUsers() {
     document.getElementById("outer-content-createNewReImbursement").style.display = "none";
     document.getElementById("outer-content-getUserById").style.display = "none";
     document.getElementById("outer-content-updateUser").style.display = "none";
+    document.getElementById("outer-content-updateUser").style.display = "none";
+    document.getElementById("outer-content-getReimByStatus").style.display = "none";
+    document.getElementById("outer-content-getReimByType").style.display = "none";
+    document.getElementById("outer-content-getAllReims").style.display = "none";
+    document.getElementById("outer-content-updateReims").style.display = "none";
     document.getElementById("outer-content-getAllUsers").style.display="block";
 }
 function toCreateNewReimb() {
     document.getElementById("indexContentDiv").style.display="none";
     document.getElementById("outer-content-getAllUsers").style.display="none"; 
     document.getElementById("outer-content-getUserById").style.display = "none"; 
-    document.getElementById("outer-content-updateUser").style.display = "none"; 
+    document.getElementById("outer-content-updateUser").style.display = "none";
+    document.getElementById("outer-content-getReimByStatus").style.display = "none";
+    document.getElementById("outer-content-getReimByType").style.display = "none";
+    document.getElementById("outer-content-getAllReims").style.display = "none";
+    document.getElementById("outer-content-updateReims").style.display = "none";
     document.getElementById("outer-content-createNewReImbursement").style.display = "block";
     
 }
@@ -33,6 +46,10 @@ function toGetUserById() {
     document.getElementById("outer-content-getAllUsers").style.display="none";   
     document.getElementById("outer-content-createNewReImbursement").style.display = "none";
     document.getElementById("outer-content-updateUser").style.display = "none";
+    document.getElementById("outer-content-getReimByStatus").style.display = "none";
+    document.getElementById("outer-content-getReimByType").style.display = "none";
+    document.getElementById("outer-content-getAllReims").style.display = "none";
+    document.getElementById("outer-content-updateReims").style.display = "none";
     document.getElementById("outer-content-getUserById").style.display = "block";
     
 }
@@ -41,7 +58,53 @@ function toUpdateUser() {
     document.getElementById("outer-content-getAllUsers").style.display="none";   
     document.getElementById("outer-content-createNewReImbursement").style.display = "none";
     document.getElementById("outer-content-getUserById").style.display = "none";
-    document.getElementById("outer-content-updateUser").style.display = "block";
-    
+    document.getElementById("outer-content-getReimByStatus").style.display = "none";
+    document.getElementById("outer-content-getReimByType").style.display = "none";
+    document.getElementById("outer-content-getAllReims").style.display = "none";
+    document.getElementById("outer-content-updateReims").style.display = "none";
+    document.getElementById("outer-content-updateUser").style.display = "block";   
 }
-
+function toGetReimByStatus() {
+    document.getElementById("indexContentDiv").style.display="none";
+    document.getElementById("outer-content-getAllUsers").style.display="none";   
+    document.getElementById("outer-content-createNewReImbursement").style.display = "none";
+    document.getElementById("outer-content-getUserById").style.display = "none";
+    document.getElementById("outer-content-updateUser").style.display = "none";
+    document.getElementById("outer-content-getReimByType").style.display = "none";
+    document.getElementById("outer-content-getAllReims").style.display = "none";
+    document.getElementById("outer-content-updateReims").style.display = "none";
+    document.getElementById("outer-content-getReimByStatus").style.display = "block";   
+}
+function toGetReimByType() {
+    document.getElementById("indexContentDiv").style.display="none";
+    document.getElementById("outer-content-getAllUsers").style.display="none";   
+    document.getElementById("outer-content-createNewReImbursement").style.display = "none";
+    document.getElementById("outer-content-getUserById").style.display = "none";
+    document.getElementById("outer-content-updateUser").style.display = "none";
+    document.getElementById("outer-content-getReimByStatus").style.display = "none";
+    document.getElementById("outer-content-getAllReims").style.display = "none";
+    document.getElementById("outer-content-updateReims").style.display = "none";
+    document.getElementById("outer-content-getReimByType").style.display = "block";   
+}
+function toGetAllReims() {
+    document.getElementById("indexContentDiv").style.display="none";
+    document.getElementById("outer-content-getAllUsers").style.display="none";   
+    document.getElementById("outer-content-createNewReImbursement").style.display = "none";
+    document.getElementById("outer-content-getUserById").style.display = "none";
+    document.getElementById("outer-content-updateUser").style.display = "none";
+    document.getElementById("outer-content-getReimByStatus").style.display = "none";
+    document.getElementById("outer-content-getReimByType").style.display = "none";
+    document.getElementById("outer-content-updateReims").style.display = "none"; 
+    document.getElementById("outer-content-getAllReims").style.display = "block";  
+}
+function toUpdateReims() {
+    document.getElementById("indexContentDiv").style.display="none";
+    document.getElementById("outer-content-getAllUsers").style.display="none";   
+    document.getElementById("outer-content-createNewReImbursement").style.display = "none";
+    document.getElementById("outer-content-getUserById").style.display = "none";
+    document.getElementById("outer-content-updateUser").style.display = "none";
+    document.getElementById("outer-content-getReimByStatus").style.display = "none";
+    document.getElementById("outer-content-getReimByType").style.display = "none";
+    document.getElementById("outer-content-getAllReims").style.display = "none";  
+    document.getElementById("outer-content-updateReims").style.display = "block";  
+}
